fad6fa9fbc7edff22e7baa3cb7254e51f294ee2c
PaddyMc <paddymchale@hotmail.com>
2022-02-08 06:58:22 +0000

    feat(simulation): add unit tests to the simulation framework

--- x/did/simulation/operations.go
+++ x/did/simulation/operations.go
@@ -3,7 +3,10 @@ package simulation
 import (
 	"math/rand"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
@@ -25,23 +28,6 @@ const (
 	// this line is used by starport scaffolding # simapp/module/const
 )
 
-// GenerateGenesisState creates a randomized GenState of the module
-func GenerateGenesisState(simState *module.SimulationState) {
-}
-
-// ProposalContents doesn't return any content functions for governance proposals
-func ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
-	return nil
-}
-
-// RandomizedParams creates randomized  param changes for the simulator
-func RandomizedParams(_ *rand.Rand) []simtypes.ParamChange {
-	return []simtypes.ParamChange{}
-}
-
-// RegisterStoreDecoder registers a decoder
-func RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
-
 // WeightedOperations returns the all the gov module operations with their respective weights.
 func WeightedOperations(simState module.SimulationState, didKeeper keeper.Keeper, bk did.BankKeeper, ak did.AccountKeeper) []simtypes.WeightedOperation {
 	operations := make([]simtypes.WeightedOperation, 0)
@@ -60,4 +46,68 @@ func WeightedOperations(simState module.SimulationState, didKeeper keeper.Keeper
 	// this line is used by starport scaffolding # simapp/module/operation
 
 	return operations
+}
+
+// SimulateMsgCreateDidDocument simulates a MsgCreateDidDocument message
+func SimulateMsgCreateDidDocument(k keeper.Keeper, bk did.BankKeeper, ak did.AccountKeeper) simtypes.Operation {
+	return func(
+		r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		didOwner, _ := simtypes.RandomAcc(r, accs)
+		ownerAddress := didOwner.Address.String()
+		didID := did.NewChainDID(ctx.ChainID(), ownerAddress)
+		vmID := didID.NewVerificationMethodID(ownerAddress)
+		vmType := did.EcdsaSecp256k1VerificationKey2019
+		auth := did.NewVerification(
+			did.NewVerificationMethod(
+				vmID,
+				didID,
+				did.NewPublicKeyMultibase(didOwner.PubKey.Bytes()),
+				vmType,
+			),
+			[]string{did.Authentication},
+			nil,
+		)
+
+		didDoc, found := k.GetDidDocument(ctx, []byte(didID))
+		d, _ := did.NewDidDocument(didID.String(), did.WithVerifications(auth))
+		msg := did.NewMsgCreateDidDocument(
+			didID.String(),
+			did.Verifications{auth},
+			did.Services{},
+			ownerAddress,
+		)
+
+		if found {
+			// check the store matches the input
+			if !didDoc.Equal(d) {
+				// return an error that will stop simulation
+				return simtypes.NoOpMsg(
+						did.ModuleName,
+						TypeMsgCreateDidDocument,
+						"did found but not matched",
+					),
+					nil,
+					sdkerrors.Wrapf(sdkerrors.ErrNotFound, "did found but not matched")
+
+			}
+			return simtypes.NoOpMsg(did.ModuleName, TypeMsgCreateDidDocument, "dids found"), nil, nil
+		}
+
+		txCtx := simulation.OperationInput{
+			R:               r,
+			App:             app,
+			TxGen:           simappparams.MakeTestEncodingConfig().TxConfig,
+			Cdc:             nil,
+			Msg:             msg,
+			MsgType:         TypeMsgCreateDidDocument,
+			Context:         ctx,
+			SimAccount:      didOwner,
+			AccountKeeper:   ak,
+			Bankkeeper:      bk,
+			ModuleName:      did.ModuleName,
+			CoinsSpentInMsg: sdk.NewCoins(),
+		}
+		return simulation.GenAndDeliverTxWithRandFees(txCtx)
+	}
 }
