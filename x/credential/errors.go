831fbb9c9720e777227c2c12627ed9c673f638eb
Andrea Giacobino <no.andrea@gmail.com>
2022-05-29 22:10:13 +0200

    test: add seed credential schema+vocab

--- x/credential/errors.go
+++ x/credential/errors.go
@@ -6,7 +6,7 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// x/did module sentinel errors
+// x/credential module sentinel errors
 // TODO update error messages
 var (
 	ErrCredentialDefinitionFound     = sdkerrors.Register(ModuleName, 2100, "credential definition found")
